e57aed085a9ca2480048ce22e00682264a4c8afb
iamEvgen <evgen.alexa@gmail.com>
2022-03-17 18:33:14 +0300

    ready js practice lessons

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.map(person => {
+        let age;
+        const currentYear = new Date().getFullYear();
+        person.yearOfDeath ? age = person.yearOfDeath - person.yearOfBirth : age = currentYear - person.yearOfBirth;
+        person.age = age;
+    })
+    people.sort((person1, person2) => person1.age > person2.age ? -1 : 1);
+    people.map(person => delete person.age);
+    console.log(people);
+    return people[0];
 };
 
 // Do not edit below this line
